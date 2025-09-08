a4e508240ee6c745a58203f91d132fc33b890752
DannyGuevara1 <guevarad170@gmail.com>
2025-09-07 21:00:20 -0600

    refactor(server): use app module and remove unused code
    Refactor server setup to use app module and improve modularity:
    src/server.ts: Restructure to import app module for routes and middleware.
    src/app.ts: Update to centralize Express app configuration.
    Remove unused middleware and legacy code from server setup.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,7 @@
-import express from 'express'
-import { PrismaClient } from '../generated/prisma/client.js'
+import app from './app.ts'
 import 'dotenv/config'
 
-const app = express()
-app.use(express.json())
-
-const prisma = new PrismaClient()
-
 const PORT = process.env.PORT || 3000
-
-app.get('/ping', (req, res) => {
-    res.send('ping')
-})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
