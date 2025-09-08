2bee4b8ebbef2c1d2ab5d72e1ae2962c94411756
DannyGuevara1 <guevarad170@gmail.com>
2025-09-07 20:57:15 -0600

    feat(app): add Express app with middleware and API routes
    Add Express app configuration in app.ts with middleware and API routes:
    - src/app.ts: Configure Express with CORS, body-parser.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,35 @@
 import express from 'express'
-// import cors from 'cors'
-// import helmet from 'helmet'
-// import morgan from 'morgan'
+import cors from 'cors'
+import { 
+    userRouter
+} from './api/components/user/index.ts'
+import { 
+    subscriptionRouter
+} from './api/components/subscription/index.ts'
+import { 
+    currencyRouter
+} from './api/components/currency/index.ts'
+import { 
+    categoryRouter
+} from './api/components/category/index.ts'
+/*
+Todo: configurar logger como morgan
+investigar sobre la mejor manera de manejar errores en express
+investigar sobre helmet para seguridad
+investigar sobre rate-limiter-flexible para limitar peticiones
+y ante todo buscar la buenas practicas y estandares profesionales
+de desarrollo con express y typescript
+*/
+const app = express()
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+const apiBasePath = '/api/v1'
+app.use(apiBasePath + '/users', userRouter)
+app.use(apiBasePath + '/subscriptions', subscriptionRouter)
+app.use(apiBasePath + '/currencies', currencyRouter)
+app.use(apiBasePath + '/categories', categoryRouter)
+
+export default app
