aede242915268de05274add696c2866c2693e346
DannyGuevara1 <guevarad170@gmail.com>
2025-09-08 20:47:51 -0600

    fix(user.controller.ts): status code change
    The correct status 200 is returned in the getUserById function.

--- src/api/components/user/user.controller.ts
+++ src/api/components/user/user.controller.ts
@@ -20,7 +20,7 @@ export class UserController {
           res.status(404).json({ message: "User not found" });
           return;
         }
-        res.status(201).json(user);
+        res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
