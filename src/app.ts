b107fad050235ee4f30fcf02bdec8a864745cd1e
DannyGuevara1 <guevarad170@gmail.com>
2025-11-18 21:06:22 -0600

    feat(app): restructure app initialization and middleware setup

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import { 
-    userRouter
-} from './api/components/user/index.ts'
-import { 
-    subscriptionRouter
-} from './api/components/subscription/index.ts'
-import { 
-    currencyRouter
-} from './api/components/currency/index.ts'
-import { 
-    categoryRouter
-} from './api/components/category/index.ts'
+// src/app.ts
+
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import { errorHandler } from '@/middleware/error.handler.js';
+import { errorNormalizer } from '@/middleware/error.normalizer.js';
+import v1 from '@/routes/index.js';
+
 /*
-Todo: configurar logger como morgan
-investigar sobre la mejor manera de manejar errores en express
-investigar sobre helmet para seguridad
 investigar sobre rate-limiter-flexible para limitar peticiones
 y ante todo buscar la buenas practicas y estandares profesionales
 de desarrollo con express y typescript
 */
-const app = express()
+
+const app = express();
+
+const whitelist = ['http://localhost:3000'];
+
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(
+	cors({
+		origin(origin, callback) {
+			if (origin && whitelist.includes(origin)) {
+				return callback(null, true);
+			}
+			// TODO: cambiar el mensaje de error
+			return callback(new Error('Not allowed by CORS'));
+		},
+	}),
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
 
 // Routes
-const apiBasePath = '/api/v1'
-app.use(apiBasePath + '/users', userRouter)
-app.use(apiBasePath + '/subscriptions', subscriptionRouter)
-app.use(apiBasePath + '/currencies', currencyRouter)
-app.use(apiBasePath + '/categories', categoryRouter)
+app.use('/api/v1', v1);
 
-export default app
+//Error handling middleware
+app.use(errorNormalizer);
+app.use(errorHandler);
+export default app;
