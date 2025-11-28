b42d9cd714173f4de7d5fedd23c141b2de3e2749
DannyGuevara1 <guevarad170@gmail.com>
2025-11-27 20:00:31 -0600

    feat: Add initial backend architecture for authentication, user, subscription, currency, and category management, including comprehensive error handling.

--- src/repositories/user/user.repository.ts
+++ src/repositories/user/user.repository.ts
@@ -1,32 +1,43 @@
-import prismaClient from "@/config/prisma.ts";
-import { BaseRepository } from "@/repositories/base.repository.ts";
-import type { User } from "@prisma/client";
-import { uuidv7 } from "uuidv7";
+// src/repositories/user/user.repository.ts
+import type { User } from '@prisma/client';
+import type {
+	CreateUserData,
+	UpdateUserData,
+} from '@/api/components/user/user.type.js';
+import prismaClient from '@/config/prisma.js';
 
-export class UserRepository extends BaseRepository<User> {
+export default class UserRepository {
+	private readonly prisma;
 
-  constructor(prisma: typeof prismaClient) {
-    super(prisma);
-  }
+	constructor(prisma = prismaClient) {
+		this.prisma = prisma;
+	}
+	// CRUD Operations
+	async create(data: CreateUserData): Promise<User> {
+		return this.prisma.user.create({ data });
+	}
 
-  async create(data: Omit<User, "id">): Promise<User> {
-    const id = uuidv7();
-    return this.prisma.user.create({ data : { ...data, id } });
-  }
+	async findAll(): Promise<User[]> {
+		return this.prisma.user.findMany();
+	}
 
-  async findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
-  }
+	async findById(id: string): Promise<User | null> {
+		return this.prisma.user.findUnique({
+			where: { id },
+		});
+	}
 
-  async findById(id: string): Promise<User | null> {
-    return this.prisma.user.findUnique({ where: { id } });
-  }
+	async findByEmail(email: string): Promise<User | null> {
+		return this.prisma.user.findUnique({
+			where: { email },
+		});
+	}
 
-  async update(id: string, data: Partial<User>): Promise<User | null> {
-    return this.prisma.user.update({ where: { id }, data });
-  }
+	async update(id: string, data: UpdateUserData): Promise<User> {
+		return this.prisma.user.update({ where: { id }, data });
+	}
 
-  async delete(id: string): Promise<User | null> {
-    return this.prisma.user.delete({ where: { id } });
-  }
+	async delete(id: string): Promise<User | null> {
+		return this.prisma.user.delete({ where: { id } });
+	}
 }
