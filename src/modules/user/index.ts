6f52062e8c302483292df9041072e903145edbbe
DannyGuevara1 <guevarad170@gmail.com>
2025-12-03 20:42:41 -0600

    feat(user): export userRouter and UserService from the module index

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,5 +1,8 @@
-//~ barrel is src/api/components/user/index.ts
+//~ barrel is src/modules/user/index.ts
 export * from '@/modules/user/user.controller.js';
 export * from '@/modules/user/user.dto.js';
 export { default as userRouter } from '@/modules/user/user.routes.js';
 export * from '@/modules/user/user.type.js';
+export { default as UserService } from '@/modules/user/user.service.js';
+export * from '@/modules/user/user.service.js';
+
