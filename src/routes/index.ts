ef6619b1757a13bd5d6e56298ee171b3d9026c9a
DannyGuevara1 <guevarad170@gmail.com>
2025-12-20 17:51:38 -0600

    feat(routes): Add authentication middleware.
    - /users
    - /currencies
    - /subscriptions
    - /categories

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 // src/routes/index.ts
 import express, { type Router } from 'express';
+import { authMiddleware } from '@/modules/auth/index.js';
 import { containerPromise } from '@/shared/container/container.js';
 
 const container = await containerPromise;
@@ -13,10 +14,9 @@ const authRouter = container.resolve<express.Router>('authRoutes');
 
 const v1: Router = express.Router();
 
-v1.use('/users', userRouter);
-v1.use('/currencies', currencyRouter);
-v1.use('/subscriptions', subscriptionRouter);
-v1.use('/categories', categoryRouter);
+v1.use('/users', authMiddleware, userRouter);
+v1.use('/currencies', authMiddleware, currencyRouter);
+v1.use('/subscriptions', authMiddleware, subscriptionRouter);
+v1.use('/categories', authMiddleware, categoryRouter);
 v1.use('/auth', authRouter);
-
 export default v1;
