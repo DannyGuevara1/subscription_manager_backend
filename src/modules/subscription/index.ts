f0cc90d639cd4af97fca0eba3200425641dec30b
DannyGuevara1 <guevarad170@gmail.com>
2025-12-26 12:57:08 -0600

    refactor(subscription): reorganize exports in subscription module for clarity

--- src/modules/subscription/index.ts
+++ src/modules/subscription/index.ts
@@ -1,5 +1,8 @@
 // src/modules/subscription/index.ts
 
+export { default as SubscriptionController } from '@/modules/subscription/subscription.controller.js';
 export * from '@/modules/subscription/subscription.dto.js';
 export { default as SubscriptionRepository } from '@/modules/subscription/subscription.repository.js';
 export { default as subscriptionRouter } from '@/modules/subscription/subscription.routes.js';
+export { default as SubscriptionService } from '@/modules/subscription/subscription.service.js';
+export * from '@/modules/subscription/subscription.type.js';
