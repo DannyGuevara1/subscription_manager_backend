6f66670175efb0d64a08f013636b97a43c16a3e4
DannyGuevara1 <guevarad170@gmail.com>
2025-12-26 12:57:57 -0600

    refactor(currency): reorganize exports in currency module for clarity

--- src/modules/currency/index.ts
+++ src/modules/currency/index.ts
@@ -1,1 +1,6 @@
+export { default as CurrencyController } from '@/modules/currency/currency.controller.js';
+export * from '@/modules/currency/currency.dto.js';
+export { default as CurrencyRepository } from '@/modules/currency/currency.repository.js';
 export { default as currencyRouter } from '@/modules/currency/currency.routes.js';
+export { default as CurrencyService } from '@/modules/currency/currency.service.js';
+export * from '@/modules/currency/currency.type.js';
