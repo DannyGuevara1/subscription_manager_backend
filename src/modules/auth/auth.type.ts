82093fd807c5a42baad0a72b8ce53318ea76950f
DannyGuevara1 <guevarad170@gmail.com>
2025-12-26 19:48:51 -0600

    refactor(auth): update user type import in AuthLoginResponse for consistency

--- src/modules/auth/auth.type.ts
+++ src/modules/auth/auth.type.ts
@@ -1,11 +1,11 @@
 // src/modules/auth/auth.type.ts
 import type { User } from '@prisma/client';
-import type { SafeUserDto } from '@/modules/user/index.js';
+import type { SafeUserAuthDto } from '@/modules/auth/index.js';
 export type AuthUser = Pick<User, 'id' | 'email' | 'name'>;
 
 export interface AuthLoginResponse {
 	accessToken: string;
-	user: SafeUserDto;
+	user: SafeUserAuthDto;
 }
 
 export interface JWTPayload {
