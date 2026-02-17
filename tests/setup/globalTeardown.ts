70076f0cbabb5ca2b223f3160f57e724b2fb1d44
DannyGuevara1 <guevarad170@gmail.com>
2026-02-16 19:43:48 -0600

    refactor(tests): remove global setup and teardown scripts along with test container management

--- tests/setup/globalTeardown.ts
+++
@@ -1,6 +0,0 @@
-import { stopContainers } from './testContainers.js';
-
-export default async function globalTeardown() {
-	console.log('\n🧹 Limpieza global de tests\n');
-	await stopContainers();
-}
