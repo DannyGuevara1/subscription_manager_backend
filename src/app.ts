b1de31a7c7224b46c32be2adee24f01b345a0c11
DannyGuevara1 <guevarad170@gmail.com>
2026-03-13 21:43:23 -0600

    fix(app): set JSON request body size limit to 100kb

--- src/app.ts
+++ src/app.ts
@@ -65,7 +65,7 @@ app.use(
 if (process.env.NODE_ENV !== 'test') {
 	app.use(globalLimiter);
 }
-app.use(express.json());
+app.use(express.json({ limit: '100kb' }));
 app.use(helmet());
 app.use(cookieParser());
 app.use(responseTime());
