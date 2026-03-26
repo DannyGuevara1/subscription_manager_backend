514d37d987b01f9228f7014176d11eaa12a09c3c
DannyGuevara1 <guevarad170@gmail.com>
2026-03-25 23:04:42 -0600

    feat(category): add offset pagination schemas for category management

--- src/modules/category/category.dto.ts
+++ src/modules/category/category.dto.ts
@@ -1,5 +1,19 @@
 import { z } from 'zod';
 
+// SCHEMAS FOR VALIDATION QUERIES PARAMS.
+export const categoryOffsetPaginationQuerySchema = z.object({
+	page: z.coerce
+		.number({ error: 'Page must be a number' })
+		.int({ error: 'Page must be an integer' })
+		.min(1, 'Page must be at least 1')
+		.default(1),
+	limit: z.coerce
+		.number({ error: 'Limit must be a number' })
+		.int({ error: 'Limit must be an integer' })
+		.min(1, 'limit must be at least 1')
+		.max(100, 'limit must be at most 100')
+		.default(10),
+});
 //SCHEMAS
 export const createCategorySchema = z.object({
 	name: z.string().min(1).max(100),
@@ -27,6 +41,10 @@ export const categoryParamsRequestSchema = z.object({
 	params: categoryParamsSchema,
 });
 
+export const categoryOffsetPaginationRequestSchema = z.object({
+	query: categoryOffsetPaginationQuerySchema,
+});
+
 //RESPONSE DTOs
 export const safeCategorySchema = z.object({
 	id: z.number(),
@@ -39,3 +57,6 @@ export type SafeCategoryDto = z.infer<typeof safeCategorySchema>;
 // Infer the TypeScript types from the Zod schemas
 export type CreateCategoryDto = z.infer<typeof createCategorySchema>;
 export type UpdateCategoryDto = z.infer<typeof updateCategorySchema>;
+export type CategoryOffsetPaginationParamsDto = z.infer<
+	typeof categoryOffsetPaginationQuerySchema
+>;
