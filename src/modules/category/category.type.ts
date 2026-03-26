c9dc1f90f9eadab0619d1c80c010bc63eeed07ce
DannyGuevara1 <guevarad170@gmail.com>
2026-03-25 23:04:32 -0600

    feat(category): add offset pagination interfaces for category management

--- src/modules/category/category.type.ts
+++ src/modules/category/category.type.ts
@@ -12,3 +12,21 @@ export interface CreateCategoryData {
 export interface UpdateCategoryData {
 	name?: string;
 }
+
+export interface offsetPaginationOptions {
+	offset: number;
+	limit: number;
+}
+
+export interface CategoryDomain {
+	id: number;
+	userId: string;
+	name: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export interface categoryOffsetPaginationResult {
+	categories: CategoryDomain[];
+	totalItems: number;
+}
