feb320be2050813546049868228b216cb936873d
DannyGuevara1 <guevarad170@gmail.com>
2026-03-28 21:02:30 -0600

    feat(types): add custom request types for enhanced type safety in Express

--- src/shared/types/express.types.ts
+++ src/shared/types/express.types.ts
@@ -1,3 +1,4 @@
+import type { Request } from 'express';
 import type { JWTPayload } from '@/modules/auth/index.js';
 
 declare global {
@@ -7,5 +8,20 @@ declare global {
 		}
 	}
 }
+
+export type RequestWithQuery<T> = Request<unknown, unknown, unknown, T>;
+
+// 2. Si solo necesitas tipar el Body (Ej: para crear un usuario)
+export type RequestWithBody<T> = Request<unknown, unknown, T, unknown>;
+
+// 3. Si solo necesitas tipar los Params de la URL (Ej: /users/:id)
+export type RequestWithParams<T> = Request<T, unknown, unknown, unknown>;
+
+// 4. El "Todo en uno" por si necesitas combinar (Ej: Body + Params)
+export type TypedRequest<
+	Body = unknown,
+	Query = unknown,
+	Params = unknown,
+> = Request<Params, unknown, Body, Query>;
 
 export {};
