534c766ee4ea958da6cefe07b60eb102788c123f
DannyGuevara1 <guevarad170@gmail.com>
2026-03-31 23:35:44 -0600

    feat(user): add user domain and pagination interfaces for improved user management

--- src/modules/user/user.type.ts
+++ src/modules/user/user.type.ts
@@ -24,3 +24,29 @@ export interface UpdateUserData {
 export interface UpdateUserRoleData {
 	role: Role;
 }
+
+export interface UserDomain {
+	id: string;
+	name: string | null;
+	email: string;
+	password: string;
+	primaryCurrencyCode: string;
+	role: Role;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export interface UserOffsetPaginationInput {
+	page: number;
+	limit: number;
+}
+
+export interface UserOffsetPaginationOptions {
+	offset: number;
+	limit: number;
+}
+
+export interface UserOffsetPaginationResult {
+	users: UserDomain[];
+	totalItems: number;
+}
