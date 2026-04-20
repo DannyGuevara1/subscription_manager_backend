f200c420bb8645349468dcf6409915dcd9ff7265
DannyGuevara1 <guevarad170@gmail.com>
2026-04-20 16:02:05 -0600

    refactor(routes): update dashboard routes to use catchAsync for error handling and bind DashboardController methods

--- src/modules/dashboard/dashboard.routes.ts
+++ src/modules/dashboard/dashboard.routes.ts
@@ -1,14 +1,17 @@
 import type { Router as ExpressRouter } from 'express';
 import { Router } from 'express';
 import { authorize } from '@/shared/middleware/authorize.js';
+import { catchAsync } from '@/shared/utils/catch.async.js';
+import DashboardController from '@/modules/dashboard/dashboard.controller.js';
 
 export const DASHBOARD_PATH = '/dashboard';
 
-export default function dashboardRoutes(): ExpressRouter {
+export default function dashboardRoutes(dashboardController: DashboardController): ExpressRouter {
 	const router = Router();
 
 	// GET
-	router.get('/summary', authorize('ADMIN', 'USER'));
+	router.get('/summary', authorize('ADMIN', 'USER'),
+	catchAsync(dashboardController.getSummary.bind(dashboardController)));
 
 	return router;
 }
